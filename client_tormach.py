71e2ea056ea7c65171e8c880ea54e0bab6ef130f
hehonglu123 <hehonglu123@126.com>
2021-10-06 12:48:29 -0400

    template

--- client_tormach.py
+++ client_tormach.py
@@ -4,7 +4,8 @@ import numpy as np
 import time,traceback, sys, yaml
 from importlib import import_module
 from general_robotics_toolbox import *
-
+sys.path.append('toolbox')
+from vel_emulate_sub import EmulatedVelocityControl
 
 def fwd(q):	
 	global robot_def
@@ -21,13 +22,47 @@ def jacobian(q):
 	global robot_def
 	return robotjacobian(robot_def,q)
 
+def move(vd, Rd):
+	global vel_ctrl
+	try:
+		w=1.
+		Kq=.01*np.eye(n)    #small value to make sure positive definite
+		KR=np.eye(3)        #gains for position and orientation error
+
+		q_cur=vel_ctrl.joint_position()
+		J=jacobian(q_cur)        #calculate current Jacobian
+		Jp=J[3:,:]
+		JR=J[:3,:] 
+		H=np.dot(np.transpose(Jp),Jp)+Kq+w*np.dot(np.transpose(JR),JR)
+
+		H=(H+np.transpose(H))/2
+
+		robot_pose=fwdkin(robot_def,q_cur.reshape((n,1)))
+		R_cur = robot_pose.R
+		ER=np.dot(R_cur,np.transpose(R_ee.R_ee(0)))
+		k,theta = R2rot(ER)
+		k=np.array(k)
+		s=np.sin(theta/2)*k         #eR2
+		wd=-np.dot(KR,s)  
+		f=-np.dot(np.transpose(Jp),vd)-w*np.dot(np.transpose(JR),wd)
+		qdot=0.5*normalize_dq(solve_qp(H, f))
+		vel_ctrl.set_velocity_command(qdot)
+
+		jobid = top.after(10, lambda: move(n, robot_def,vel_ctrl,vd))
+	except:
+		traceback.print_exc()
+	return
+
+
 def main():
-	global robot_def
+	global robot_def, vel_ctrl
 	####################Start Service and robot setup
 
 	robot_sub=RRN.SubscribeService('rr+tcp://pathpilot:11111?service=tormach_robot')
 	robot=robot_sub.GetDefaultClientWait(1)
 	state_w = robot_sub.SubscribeWire("robot_state")
+	cmd_w = robot_sub.SubscribeWire("position_command")
+	vel_ctrl = EmulatedVelocityControl(robot,state_w, cmd_w)
 
 	print(robot.robot_info.device_info.device.name+" Connected")
 
