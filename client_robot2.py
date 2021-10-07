0f1ac82173404154cae5776d1eb707f13e67799e
hehonglu123 <hehonglu123@126.com>
2021-10-07 17:11:27 -0400

    final

--- client_robot2.py
+++ client_robot2.py
@@ -137,7 +137,7 @@ def move(vd, ER):
 
 
 def comp_control(F_x_des=1.):
-	global robot_def, vel_ctrl, netft, state_w, force_reading
+	global robot_def, vel_ctrl, netft, state_w, force_reading, joint_position_reading
 
 	
 
@@ -152,7 +152,7 @@ def comp_control(F_x_des=1.):
 	force_z=-np.sin(np.pi/4)*ft[3]-np.cos(np.pi/4)*ft[4]
 	force_x=ft[-1]
 
-	force_reading.append([time.time(),force_x,force_z,torque_y])
+
 
 
 	# if np.abs(force_x)<0.15:
@@ -167,6 +167,11 @@ def comp_control(F_x_des=1.):
 	unit1=R_cur[:,0]/np.linalg.norm(R_cur[:,0])
 	theta=np.arccos(np.dot(unit1,np.array([0,1,0])))
 
+
+	###data recording
+	force_reading.append([time.time(),force_x,force_z,torque_y])
+	joint_position_reading.append(q_cur)
+
 	###get FT_COC
 	z_cur=pose.p[-1]
 	d_EE_COC=z_cur-(z_init-d_EE_TCP)
@@ -178,9 +183,10 @@ def comp_control(F_x_des=1.):
 
 	damping_coeff=max(np.linalg.norm(np.array([force_z,force_x,torque_y]))/2.,1)
 
-	v_y_EE, v_z_EE, omega_EE = gen_damper_control(theta, force_z, force_x, torque_y, b_x=damping_coeff*5000., b_z=damping_coeff*1000., b_omega=damping_coeff*100., d_FT_COC=d_FT_COC, d_EE_COC=d_EE_COC, F_x_des=F_x_des)
+	v_y_EE, v_z_EE, omega_EE = gen_damper_control(theta, force_z, force_x, torque_y, b_x=damping_coeff*2000., b_z=damping_coeff*1000., b_omega=damping_coeff*10., d_FT_COC=d_FT_COC, d_EE_COC=d_EE_COC, F_x_des=F_x_des)
 	print('v_y: ', v_y_EE)
 	print('v_z: ', v_z_EE)
+	print('theta: ', theta)
 	print('force_x: ',force_x)
 	print('      ')
 
@@ -189,10 +195,36 @@ def comp_control(F_x_des=1.):
 	ER=Rx(-omega_EE)
 	move(vd,ER)
 	
+def no_comp():
+	global robot_def, vel_ctrl, netft, state_w, force_reading, joint_position_reading, stop
+
+	res, ft, status = netft.try_read_ft_streaming(.1)
+	torque_y=-np.sin(np.pi/4)*ft[0]+np.cos(np.pi/4)*ft[1]
+	force_z=-np.sin(np.pi/4)*ft[3]-np.cos(np.pi/4)*ft[4]
+	force_x=ft[-1]
+	
+	q_cur=state_w.InValue.joint_position
+	###data recording
+	force_reading.append([time.time(),force_x,force_z,torque_y])
+	joint_position_reading.append(q_cur)
+
+	vd=np.array([0,0,-0.01])
+	move(vd,np.eye(3))
+
+	if np.abs(force_x)>200:
+		stop=True
+
 
 
 def main():
-	global robot_def, vel_ctrl, netft, state_w, force_reading, stop
+	global robot_def, vel_ctrl, netft, state_w, force_reading, stop, joint_position_reading
+
+
+	###jogging initial pose
+	q_poffset=[ 1.57489506,  1.00884044, -0.06142472,  0.00267314,  0.61304244,  1.57120872]
+	q_orioffset=[1.57526511e+00, 8.51933448e-01, 1.53590792e-01, 1.30709195e-03, 2.78377532e-01, 1.57228823e+00]
+	q_perfect=[ 1.57480585,  0.99928189, -0.07043686,  0.00262175,  0.63853202,  1.57113588]
+
 	####################Start Service and robot setup
 
 	robot_sub=RRN.SubscribeService('rr+tcp://pi_fuse:58651?service=robot')
@@ -212,6 +244,11 @@ def main():
 
 
 	print(robot.robot_info.device_info.device.name+" Connected")
+
+
+	###jogging to intial pose
+	jog_joint(q_orioffset,0.2)
+
 
 	num_joints=len(robot.robot_info.joint_info)
 	P=np.array(robot.robot_info.chains[0].P.tolist())
@@ -233,6 +270,7 @@ def main():
 	vel_ctrl.enable_velocity_mode()
 	netft.set_tare_from_ft()
 	Fd=100.
+	joint_position_reading=[]
 	force_reading=[]
 	stop=False
 	now=time.time()
@@ -245,13 +283,14 @@ def main():
 
 	line_fz, = ax.plot([1,2], [1,2], 'b-')
 	line_fx, = ax.plot([1,2],[1,2], 'r-')
+	line_ty, = ax.plot([1,2],[1,2], 'g-')
 
 	last_time=time.time()
 	while True:
 		try:
 			# temp_time=time.time()
 			comp_control(Fd)
-
+			# no_comp()
 			if time.time()-last_time>0.01:
 				force_reading_nd=np.array(force_reading)
 				force_reading_nd[:,0]-=now
@@ -259,6 +298,10 @@ def main():
 				line_fz.set_ydata(force_reading_nd[max(0,len(force_reading)-max_idx):,1])
 				line_fx.set_xdata(force_reading_nd[max(0,len(force_reading)-max_idx):,0])
 				line_fx.set_ydata(force_reading_nd[max(0,len(force_reading)-max_idx):,2])
+				line_ty.set_xdata(force_reading_nd[max(0,len(force_reading)-max_idx):,0])
+				line_ty.set_ydata(force_reading_nd[max(0,len(force_reading)-max_idx):,3])
+
+
 				ax.relim()
 				ax.autoscale_view()
 				fig.canvas.draw()
@@ -277,13 +320,15 @@ def main():
 			break
 
 	
-	# plt.plot(force_reading_nd[:,0],force_reading_nd[:,3])
 	plt.xlabel('time (s)')
 	plt.ylabel('force (N)')
 	plt.legend(['force z','force_y'])
 	plt.show()
 
-	np.savetxt("force_data.csv", force_reading, delimiter=",")
+	print(len(force_reading),len(joint_position_reading))
+	print(force_reading[-1])
+	final_data=np.hstack((np.array(force_reading),np.array(joint_position_reading)))
+	np.savetxt("final_data"+str(int(time.time()))+".csv", final_data, delimiter=",")
 
 	# pose_p=np.array([-0.00772909, 0.66492853, 0.5176524])
 	# pose_R=np.array([[np.pi/2,0,0,0,0,0],R=np.array([[ 0,0,1],
